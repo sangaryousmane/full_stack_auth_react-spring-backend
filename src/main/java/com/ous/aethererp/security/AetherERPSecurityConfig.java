f919178413b493a0467460c56c094e63034c281f
Ousmane Sangary <sangaryousmane@gmail.com>
2026-06-19 00:22:13 +0000

    Amend config file

--- src/main/java/com/ous/aethererp/security/AetherERPSecurityConfig.java
+++ src/main/java/com/ous/aethererp/security/AetherERPSecurityConfig.java
@@ -60,7 +60,7 @@ public class AetherERPSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config=new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:8080"));
+        config.setAllowedOrigins(List.of("http://localhost:3001"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of(
                 "Authorization",
